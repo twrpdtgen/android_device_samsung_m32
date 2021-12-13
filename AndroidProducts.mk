672e556c8d77944f19f40ac2d48ec842c286a50b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-13 11:50:03 +0100

    m32: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: samsung Model: SM-M325F Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_m32.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_m32-user \
+    omni_m32-userdebug \
+    omni_m32-eng
